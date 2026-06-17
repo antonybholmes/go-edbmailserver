32588fff1450e6cd4b29513923eb58d5dd7c7f68
Antony Holmes <antony@antonyholmes.dev>
2026-06-17 13:47:04 -0400

    fix: Bug fixes and updates.

--- merge_main.sh
+++ merge_main.sh
@@ -35,7 +35,8 @@ echo ${branch}
 ./commit.sh -t "${type}" -m "${msg}" -b dev
 
 git switch main
-git merge dev -m "${type}: ${msg}"
+git merge dev -m "${type}: ${msg}" -X theirs
+git checkout dev -- version.json
 
 #git push -u origin main
 ./commit.sh -t "${type}" -m "${msg}" -b main
